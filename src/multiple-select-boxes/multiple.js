f6f886da2754039495fb9228e19ee5dcc8f25af5
Taulant Spahiu <taulantspahiu@users.noreply.github.com>
2016-08-11 23:01:38 +0200

    Add files via upload

---
+++ src/multiple-select-boxes/multiple.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  	//Chosen
+    $(".multipleChosen").chosen({
+    	placeholder_text_multiple: "What's your rating" //placeholder
+	});
+	//Select2
+	$(".multipleSelect2").select2({
+		placeholder: "What's your rating" //placeholder
+	});
+})
