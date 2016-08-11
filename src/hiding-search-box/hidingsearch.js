47485854c3f0a36b670ae80bebb3d10637bec5d9
Taulant Spahiu <taulantspahiu@users.noreply.github.com>
2016-08-12 00:27:34 +0200

    Update hidingsearch.js

--- src/hiding-search-box/hidingsearch.js
+++ src/hiding-search-box/hidingsearch.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
   //Chosen
   $(".hideBoxChosen").chosen({
       placeholder_text_single: "Are you an adult?", //placeholder
-    disable_search_threshold: 4
-	});
+      disable_search_threshold: 4
+  });
   //Select2
   $(".hideBoxSelect2").select2({
-		placeholder: "Are you an adult?", //placeholder
-    minimumResultsForSearch: 4
-	});
+  	placeholder: "Are you an adult?", //placeholder
+    	minimumResultsForSearch: 4
+  });
 })
