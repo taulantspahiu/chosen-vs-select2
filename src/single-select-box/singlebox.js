20e5af9c479f21a7ee87da449b1950a5b9c43ef0
Taulant Spahiu <taulantspahiu@users.noreply.github.com>
2016-08-11 22:39:16 +0200

    Single Select Box Sample

---
+++ src/single-select-box/singlebox.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	//Chosen
+	$(".singleChosen").chosen({
+		placeholder_text_single:"What's your rating" //placeholder
+	});	
+  //Select2
+  $(".singleSelect2").select2({
+		placeholder: "What's your rating" //placeholder
+	});	
+});
