5620642248c6d6492104a26a7d7a6fe25ffdd064
Taulant Spahiu <taulantspahiu@users.noreply.github.com>
2016-08-12 00:01:18 +0200

    Add files via upload

---
+++ src/disabled_results/dresults.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	//Chosen
+  $(".choiceChosen, .productChosen").chosen({});
+  //Logic
+  $(".choiceChosen").change(function(){
+    if($(".choiceChosen option:selected").val()=="no"){
+      $(".productChosen option[value='2']").attr('disabled',true).trigger("chosen:updated");
+      $(".productChosen option[value='1']").removeAttr('disabled',true).trigger("chosen:updated");
+    } else {
+      $(".productChosen option[value='1']").attr('disabled',true).trigger("chosen:updated");
+      $(".productChosen option[value='2']").removeAttr('disabled',true).trigger("chosen:updated");
+    }
+  })
+})
