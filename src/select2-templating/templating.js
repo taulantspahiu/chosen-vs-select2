79b9f711cc29b59aaea2e8552f020f309bcaf169
Taulant Spahiu <taulantspahiu@users.noreply.github.com>
2016-08-12 10:43:02 +0200

    Add files via upload

---
+++ src/select2-templating/templating.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  function setCurrency (currency) {
+	  if (!currency.id) { return currency.text; }
+		var $currency = $('<span class="glyphicon glyphicon-' + currency.element.value + '">' + currency.text + '</span>');
+		return $currency;
+	};
+	$(".templatingSelect2").select2({
+		placeholder: "What currency do you use?", //placeholder
+		templateResult: setCurrency,
+		templateSelection: setCurrency
+	});
+})
